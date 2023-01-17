2f8202031093e8588ea07d9f48bf2c0d21eabf66
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-01-17 11:54:13 -0300

    About View

---
+++ src/gui/util/Constrainsts.java
@@ -0,0 +1,31 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constrainsts {
+
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue.length() > max) {
+				txt.setText(newValue.substring(0, max));
+			}
+		});
+	}
+
+	public static void setTextFielDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+}
